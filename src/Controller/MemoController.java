336b19269eeb971394fd29dc1f3a97c43da9aab7
Meta <meta@email.com>
2024-07-30 22:01:37 +0900

    feat : add delete function for memo

--- src/Controller/MemoController.java
+++ src/Controller/MemoController.java
@@ -4,4 +4,5 @@ public class MemoController {
     String function1 = "Create function complete";
     String function2 = "Read function complete";
     String function3 = "Update function complete";
+    String function4 = "Delete function complete";
 }
